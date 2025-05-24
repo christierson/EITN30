a9ac576d0ee18c9381d6ef54ef9377653fe3793a
Clemens <christierson.c@gmail.com>
2025-05-24 11:15:37 +0200

    l

--- device.py
+++ device.py
@@ -2,29 +2,6 @@ import socket
 import functools
 
 
-# # MOBILE
-
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# sock.connect(("10.0.0.1", 5000))  # connect to base station on port 5000
-# sock.sendall(b"Hello Base Station!")
-# response = sock.recv(1024)
-# print("Received:", response)
-# sock.close()
-
-
-# # BASE
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server.bind(("10.0.0.1", 5000))
-# server.listen()
-
-# conn, addr = server.accept()
-# print("Connection from", addr)
-# data = conn.recv(1024)
-# print("Received:", data)
-# conn.sendall(b"Hello Mobile Unit!")
-# conn.close()
-
-
 def socket_safe(method):
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
@@ -41,9 +18,10 @@ def socket_safe(method):
     return wrapper
 
 
-class Device:
-    def __init__(self):
+class Mobile:
+    def __init__(self, ip):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((ip, 5000))
 
     @socket_safe
     def send(self, msg: str):
@@ -54,19 +32,18 @@ class Device:
         return self.sock.recv(1024)
 
 
-class Mobile(Device):
+class Base:
     def __init__(self, ip):
-        super().__init__()
-        self.sock.connect((ip, 5000))
-
-
-class Base(Device):
-    def __init__(self, ip):
-        super().__init__()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connections = []
-        self.sock
         self.sock.bind((ip, 5000))
-
-    def start(self):
         self.sock.listen()
         self.conn, self.addr = self.sock.accept()
+
+    @socket_safe
+    def send(self, msg: str):
+        self.conn.sendall(msg.encode())
+
+    @socket_safe
+    def recv(self):
+        return self.conn.recv(1024)
